8c060c757ce7808735a5970ac864ed4c1e7e3065
unknown <austinlee199723@yahoo.com>
2024-07-06 15:19:10 -0700

    setting up zapier

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,25 @@
+<script>
+	export const prerender = true;
+	let email = '';
+	let message = '';
+	let subject = '';
+	let name = '';
+
+	async function handleSubmit() {
+		const response = await fetch('/send-email', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ email, message, subject, name })
+		});
+
+		if (response.ok) {
+			console.log('Email sent successfully!');
+		} else {
+			console.error('Failed to send email.');
+		}
+	}
+</script>
+
 <div
 	class="wrapper flex w-full justify-between mt-10 sm:flex-row flex-col items-center sm:items-start"
 >
@@ -8,67 +30,82 @@
 			<p>alecwangla@gmail.com</p>
 		</div>
 	</div>
-	<form class="sm:w-1/2 w-full px-5 sm:px-0">
+	<form
+		on:submit|preventDefault="{handleSubmit}"
+		class="sm:w-1/2 w-full px-5 sm:px-0"
+		name="contact"
+		method="post"
+		data-netlify="true"
+	>
+		<input type="hidden" name="form-name" value="contact" />
+
 		<div class="relative z-0 w-full mb-5 group">
 			<input
-				type="password"
-				name="floating_password"
-				id="floating_password"
+				bind:value="{name}"
+				type="text"
+				name="name"
+				id="name"
 				class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 				placeholder=" "
 				required
 			/>
 			<label
-				for="floating_password"
+				for="name"
 				class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
 				>Name</label
 			>
 		</div>
+
 		<div class="relative z-0 w-full mb-5 group">
 			<input
-				type="password"
-				name="floating_password"
-				id="floating_password"
+				bind:value="{subject}"
+				type="text"
+				name="subject"
+				id="subject"
 				class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 				placeholder=" "
 				required
 			/>
 			<label
-				for="floating_password"
+				for="subject"
 				class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-				>Subject:</label
+				>Subject</label
 			>
 		</div>
+
 		<div class="relative z-0 w-full mb-5 group">
 			<input
+				bind:value="{email}"
 				type="email"
-				name="floating_email"
-				id="floating_email"
+				name="email"
+				id="email"
 				class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 				placeholder=" "
 				required
 			/>
 			<label
-				for="floating_email"
+				for="email"
 				class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
 				>Email</label
 			>
 		</div>
+
 		<div class="relative z-0 w-full mb-5 group">
 			<textarea
-				type="text"
-				name="floating_message"
-				id="floating_message"
+				bind:value="{message}"
+				name="message"
+				id="message"
 				class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
 				placeholder=" "
 				required
 				rows="4"></textarea>
 			<label
-				for="floating_message"
+				for="message"
 				class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
 				>Message</label
 			>
 		</div>
+
 		<button
 			type="submit"
 			class="text-black bg-blue2 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:hover:bg-blue-700 dark:focus:ring-blue-800"
